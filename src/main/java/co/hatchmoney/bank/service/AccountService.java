cb23a22a398e7a5cca80b101a7b24cf53539d65d
Phil-I-Am <phil@hatchmoney.co>
2017-06-06 09:28:10 +0100

    Update AccountService.java

--- src/main/java/co/hatchmoney/bank/service/AccountService.java
+++ src/main/java/co/hatchmoney/bank/service/AccountService.java
@@ -20,7 +20,7 @@ public class AccountService {
     }
 
     public String getBalance() {
-        String strDummyBalance = "{ \"balance\": \"1205.95\" }";
+        String strDummyBalance = "{ \"balance\": \"875.95\" }";
         return getValidatedJsonString(strDummyBalance, true);
     }
 
